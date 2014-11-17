da0cf0b686af4353994f765a7774ebe295cea762
Robert Guo <robertguo@me.com>
2014-11-17 13:24:42 -0500

    Create script.bash

---
+++ script.bash
@@ -0,0 +1,10 @@
+# install XCode
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python
+brew install mercurial
+brew install git
+brew install zsh
+brew install p7zip
+brew install scons
+brew install emacs
+brew install go
